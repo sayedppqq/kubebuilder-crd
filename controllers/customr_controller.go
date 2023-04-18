0240961c080e0a74cd09a52a67c901d81e46e45a
sayedppqq <sayedmmnn0@gmail.com>
2023-04-18 16:03:06 +0600

    first commit

---
+++ controllers/customr_controller.go
@@ -0,0 +1,76 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	appsv1 "k8s.io/api/apps/v1"
+	sayedppqqdevv1 "sayedppqq.dev/mycrd/api/v1"
+)
+
+// CustomRReconciler reconciles a CustomR object
+type CustomRReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=sayedppqq.dev.sayedppqq.dev,resources=customrs,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=sayedppqq.dev.sayedppqq.dev,resources=customrs/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=sayedppqq.dev.sayedppqq.dev,resources=customrs/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the CustomR object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
+func (r *CustomRReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+	log.WithValues("ReqName", req.Name, "ReqNamespace", req.Namespace)
+
+	// TODO(user): your logic here
+
+	// customRInstance have all data of CustomR Resources
+	var customRInstance sayedppqqdevv1.CustomR
+	if err := r.Get(ctx, req.NamespacedName, &customRInstance); err != nil {
+		log.Info("Unable to Fetch customRInstance")
+		// we'll ignore not-found errors, since they can't be fixed by an immediate
+		// requeue (we'll need to wait for a new notification), and we can get them
+		// on deleted requests.
+		return ctrl.Result{}, nil
+	}
+
+	// deploymentInstance have all data of deployment in specific namespace
+	var deploymentInstance appsv1.Deployment
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *CustomRReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&sayedppqqdevv1.CustomR{}).
+		Complete(r)
+}
